8cefdb70b90a8c9f547ec76dd92fec2af50dfb3d
Christian Iversen <ci@iversenit.dk>
2024-08-01 01:13:56 +0200

    + Add helper functions on DeviceProductData to emulate standard responses

--- src/hue/v2.rs
+++ src/hue/v2.rs
@@ -611,3 +611,27 @@ impl ResourceLink {
 pub struct TimeZone {
     pub time_zone: String,
 }
+
+impl DeviceProductData {
+    pub fn hue_color_spot() -> Self {
+        Self {
+            model_id: "LCG002".to_string(),
+            manufacturer_name: "Signify Netherlands B.V.".to_string(),
+            product_name: "Hue color spot".to_string(),
+            product_archetype: "spot_bulb".to_string(),
+            certified: true,
+            software_version: "1.104.2".to_string(),
+        }
+    }
+
+    pub fn hue_bridge_v2() -> Self {
+        Self {
+            certified: true,
+            manufacturer_name: "Signify Netherlands B.V.".to_string(),
+            model_id: "BSB002".to_string(),
+            product_archetype: "bridge_v2".to_string(),
+            product_name: "Hue Bridge".to_string(),
+            software_version: "1.60.1960149090".to_string(),
+        }
+    }
+}
