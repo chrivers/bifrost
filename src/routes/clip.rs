4928b43e4f91a9a1a33110c89fac6dfda9e3404c
Christian Iversen <ci@iversenit.dk>
2024-08-01 14:29:25 +0200

    + Implemented support for creating rooms

--- src/routes/clip.rs
+++ src/routes/clip.rs
@@ -1,10 +1,9 @@
 use axum::extract::State;
-use axum::routing::post;
 use axum::{extract::Path, response::IntoResponse, routing::get, Json, Router};
 use serde_json::Value;
 use uuid::Uuid;
 
-use crate::hue::v2::{ResourceLink, ResourceType, Scene, V2Reply};
+use crate::hue::v2::{ResourceLink, ResourceType, Room, Scene, V2Reply};
 use crate::state::AppState;
 
 async fn get_root(State(state): State<AppState>) -> impl IntoResponse {
@@ -32,6 +31,20 @@ async fn post_resource(
     log::info!("POST {rtype:?}: {req:?}");
 }
 
+async fn get_scene(State(state): State<AppState>) -> impl IntoResponse {
+    Json(V2Reply {
+        data: state.get_resources_by_type(ResourceType::Scene).await,
+        errors: vec![],
+    })
+}
+
+async fn get_room(State(state): State<AppState>) -> impl IntoResponse {
+    Json(V2Reply {
+        data: state.get_resources_by_type(ResourceType::Room).await,
+        errors: vec![],
+    })
+}
+
 async fn post_scene(
     State(state): State<AppState>,
     Json(req): Json<Value>,
@@ -43,6 +56,24 @@ async fn post_scene(
 
     log::info!("POST scene: {scene:#?}");
     let link = state.res.lock().await.add_scene(scene);
+    Ok(Json(V2Reply {
+        data: vec![link],
+        errors: vec![],
+    }))
+}
+
+async fn post_room(
+    State(state): State<AppState>,
+    Json(req): Json<Value>,
+) -> axum::response::Result<Json<V2Reply<ResourceLink>>> {
+    log::info!("POST room: {}", serde_json::to_string(&req).unwrap());
+    let scn = serde_json::from_value::<Room>(req);
+    println!("{:?}", &scn);
+    let mut room = scn.unwrap();
+    room.id_v1 = Some("/rooms/v2".to_string());
+
+    log::info!("POST room: {room:#?}");
+    let link = state.res.lock().await.add_room(room);
     Ok(Json(V2Reply {
         data: vec![link],
         errors: vec![],
@@ -78,7 +109,8 @@ async fn put_resource_id(
 pub fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(get_root))
-        .route("/scene", post(post_scene))
+        .route("/scene", get(get_scene).post(post_scene))
+        .route("/room", get(get_room).post(post_room))
         .route("/:resource", get(get_resource).post(post_resource))
         .route("/:resource/:id", get(get_resource_id).put(put_resource_id))
 }
