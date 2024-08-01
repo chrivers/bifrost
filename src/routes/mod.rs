7d461de4c6bcc936a4f7f17410c79a0f38f13918
Christian Iversen <ci@iversenit.dk>
2024-08-01 22:35:55 +0200

    + Define ApiError stub

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,11 @@
+use thiserror::Error;
+
 pub mod api;
 pub mod clip;
 pub mod eventstream;
+
+#[derive(Error, Debug)]
+pub enum ApiError {
+    #[error("Request failed: {0}")]
+    Fail(&'static str),
+}
