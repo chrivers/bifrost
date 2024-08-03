9bff623c09d626c96e55c8c55edc7f4a8d95ebe0
Christian Iversen <ci@iversenit.dk>
2024-08-03 10:23:05 +0200

    * Moved clippy lint section to lib.rs, and tightened bounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+#![warn(
+    clippy::all,
+    clippy::correctness,
+    clippy::pedantic,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::perf,
+    clippy::style
+)]
+#![allow(
+    clippy::cargo_common_metadata,
+    clippy::multiple_crate_versions,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::module_name_repetitions
+)]
+
 pub mod error;
 pub mod hue;
 pub mod z2m;
