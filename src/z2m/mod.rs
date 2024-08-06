3ec18149aef746b5d517eb5268ebcd22fbe048fb
Christian Iversen <ci@iversenit.dk>
2024-08-06 12:23:47 +0200

    * Reorganize z2m::Client to dispatch to separate functions

--- src/z2m/mod.rs
+++ src/z2m/mod.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use futures::StreamExt;
-use serde_json::json;
+use serde_json::{json, Value};
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use tokio_tungstenite::{connect_async, tungstenite, MaybeTlsStream, WebSocketStream};
@@ -21,7 +21,7 @@ use crate::error::ApiResult;
 use crate::hue::scene_icons;
 use crate::resource::AuxData;
 use crate::resource::Resources;
-use crate::z2m::api::{Message, Other};
+use crate::z2m::api::Message;
 use crate::z2m::update::DeviceUpdate;
 
 pub struct Client {
@@ -30,67 +30,201 @@ pub struct Client {
     map: HashMap<String, Uuid>,
 }
 
-fn handle_light(uuid: &Uuid, res: &mut Resources, obj: &Other) -> ApiResult<()> {
-    let upd: DeviceUpdate = serde_json::from_value(obj.payload.clone())?;
-
-    res.update_light(uuid, move |light| {
-        if let Some(state) = &upd.state {
-            light.on.on = (*state).into();
-        }
-
-        if let Some(b) = upd.brightness {
-            light.dimming.brightness = b / 254.0 * 100.0;
-        }
-
-        light.color_mode = upd.color_mode;
-
-        if let Some(ct) = upd.color_temp {
-            light.color_temperature.mirek = ct;
-            /* light.color_temperature.mirek_valid = true; */
-        }
-
-        if let Some(col) = upd.color {
-            light.color.xy = col.xy;
-            /* light.color_temperature.mirek_valid = false; */
-        }
-    })?;
-
-    Ok(())
-}
-
-fn handle_grouped_light(uuid: &Uuid, res: &mut Resources, obj: &Other) -> ApiResult<()> {
-    let upd: DeviceUpdate = serde_json::from_value(obj.payload.clone())?;
-
-    res.update_grouped_light(uuid, move |glight| {
-        if let Some(state) = &upd.state {
-            glight.on.on = (*state).into();
-        }
-
-        if let Some(b) = upd.brightness {
-            glight.dimming.brightness = b / 254.0 * 100.0;
-        }
-
-        /* glight.color_mode = upd.color_mode; */
-
-        if let Some(ct) = upd.color_temp {
-            glight.color_temperature.mirek = ct;
-            /* glight.color_temperature.mirek_valid = true; */
-        }
-
-        if let Some(col) = upd.color {
-            glight.color.xy = col.xy;
-            /* glight.color_temperature.mirek_valid = false; */
-        }
-    })?;
-
-    Ok(())
-}
-
 impl Client {
     pub async fn new(conn: &str, state: Arc<Mutex<Resources>>) -> ApiResult<Self> {
         let (socket, _) = connect_async(conn).await?;
         let map = HashMap::new();
         Ok(Self { socket, state, map })
+    }
+
+    pub async fn add_light(&mut self, dev: &crate::z2m::api::Device) -> ApiResult<()> {
+        let name = &dev.friendly_name;
+
+        let link_device = ResourceType::Device.deterministic(&dev.ieee_address);
+        let link_light = ResourceType::Light.deterministic(&dev.ieee_address);
+
+        let dev = Device {
+            product_data: DeviceProductData::hue_color_spot(),
+            metadata: Metadata::spot_bulb(name),
+            identify: json!({}),
+            services: vec![link_light.clone()],
+        };
+
+        self.map.insert(name.to_string(), link_light.rid);
+
+        let mut res = self.state.lock().await;
+        let mut light = Light::new(res.next_idv1(), link_device.clone());
+        light.metadata.name = name.to_string();
+
+        res.aux
+            .insert(link_light.rid, AuxData::new().with_topic(name));
+        res.add(&link_device, Resource::Device(dev))?;
+        res.add(&link_light, Resource::Light(light))?;
+        drop(res);
+
+        Ok(())
+    }
+
+    pub async fn add_group(&mut self, grp: &crate::z2m::api::Group) -> ApiResult<()> {
+        let link_room = ResourceType::Room.deterministic(grp.id);
+        let link_glight = ResourceType::GroupedLight.deterministic(grp.id);
+
+        let children = grp
+            .members
+            .iter()
+            .map(|f| ResourceType::Device.deterministic(&f.ieee_address))
+            .collect();
+
+        let mut services = vec![link_glight.clone()];
+
+        let topic = grp.friendly_name.to_string();
+
+        let mut res = self.state.lock().await;
+
+        for scn in &grp.scenes {
+            let scene = Scene {
+                actions: vec![],
+                auto_dynamic: false,
+                group: link_room.clone(),
+                id_v1: Some("/scene/blob".to_string()),
+                metadata: SceneMetadata {
+                    appdata: None,
+                    image: guess_scene_icon(&scn.name),
+                    name: scn.name.to_string(),
+                },
+                palette: json!({
+                    "color": [],
+                    "dimming": [],
+                    "color_temperature": [],
+                    "effects": [],
+                }),
+                recall: None,
+                speed: 0.5,
+                status: Some(SceneStatus {
+                    active: "inactive".to_string(),
+                }),
+            };
+
+            let link_scene = ResourceType::Scene.deterministic((grp.id, scn.id));
+
+            res.aux.insert(
+                link_scene.rid,
+                AuxData::new().with_topic(&topic).with_index(scn.id),
+            );
+
+            services.push(link_scene.clone());
+            res.add(&link_scene, Resource::Scene(scene))?;
+        }
+
+        let room = Room {
+            id_v1: Some(format!("/room/{}", grp.id)),
+            children,
+            metadata: Metadata::room(RoomArchetypes::Computer, &topic),
+            services,
+        };
+
+        self.map.insert(topic, link_glight.rid);
+        res.add(&link_room, Resource::Room(room))?;
+
+        let glight = GroupedLight {
+            alert: json!({
+                "action_values": [],
+            }),
+            color: LightColor::dummy(),
+            color_temperature: ColorTemperature::dummy(),
+            color_temperature_delta: json!({}),
+            dimming: Dimming {
+                brightness: 100.0,
+                min_dim_level: None,
+            },
+            dimming_delta: json!({}),
+            dynamics: json!({}),
+            id_v1: Some(format!("/groups/{}", grp.id)),
+            on: On { on: true },
+            owner: link_room,
+            signaling: json!({
+                "signal_values": [],
+            }),
+        };
+
+        res.add(&link_glight, Resource::GroupedLight(glight))?;
+        drop(res);
+
+        Ok(())
+    }
+
+    pub async fn handle_update(&self, rid: &Uuid, payload: Value) -> ApiResult<()> {
+        let mut res = self.state.lock().await;
+        let upd: DeviceUpdate = serde_json::from_value(payload)?;
+
+        match res.get_resource_by_id(rid)?.obj {
+            Resource::Light(_) => {
+                if let Err(e) = Self::handle_update_light(&mut res, rid, &upd) {
+                    log::error!("FAIL: {e:?} in {upd:?}");
+                }
+            }
+            Resource::GroupedLight(_) => {
+                if let Err(e) = Self::handle_update_grouped_light(&mut res, rid, &upd) {
+                    log::error!("FAIL: {e:?} in {upd:?}");
+                }
+            }
+            _ => {}
+        }
+        drop(res);
+
+        Ok(())
+    }
+
+    fn handle_update_light(res: &mut Resources, uuid: &Uuid, upd: &DeviceUpdate) -> ApiResult<()> {
+        res.update_light(uuid, move |light| {
+            if let Some(state) = &upd.state {
+                light.on.on = (*state).into();
+            }
+
+            if let Some(b) = upd.brightness {
+                light.dimming.brightness = b / 254.0 * 100.0;
+            }
+
+            light.color_mode = upd.color_mode;
+
+            if let Some(ct) = upd.color_temp {
+                light.color_temperature.mirek = ct;
+                /* light.color_temperature.mirek_valid = true; */
+            }
+
+            if let Some(col) = upd.color {
+                light.color.xy = col.xy;
+                /* light.color_temperature.mirek_valid = false; */
+            }
+        })
+    }
+
+    fn handle_update_grouped_light(
+        res: &mut Resources,
+        uuid: &Uuid,
+        upd: &DeviceUpdate,
+    ) -> ApiResult<()> {
+        res.update_grouped_light(uuid, move |glight| {
+            if let Some(state) = &upd.state {
+                glight.on.on = (*state).into();
+            }
+
+            if let Some(b) = upd.brightness {
+                glight.dimming.brightness = b / 254.0 * 100.0;
+            }
+
+            /* glight.color_mode = upd.color_mode; */
+
+            if let Some(ct) = upd.color_temp {
+                glight.color_temperature.mirek = ct;
+                /* glight.color_temperature.mirek_valid = true; */
+            }
+
+            if let Some(col) = upd.color {
+                glight.color.xy = col.xy;
+                /* glight.color_temperature.mirek_valid = false; */
+            }
+        })
     }
 
     pub async fn event_loop(mut self) -> ApiResult<()> {
@@ -115,8 +249,6 @@ impl Client {
                 continue;
             };
 
-            let mut res = self.state.lock().await;
-
             #[allow(unused_variables)]
             match msg {
                 /* Message::BridgeInfo(ref obj) => { */
@@ -137,32 +269,12 @@ impl Client {
                                     || (id == "LCG002")
                                     || (id == "TRADFRI bulb E27 CWS 806lm") =>
                             {
-                                println!("{:?}", dev.ieee_address);
-                                println!("{:?}", dev.friendly_name);
-
-                                let name = &dev.friendly_name;
-
-                                let link_device =
-                                    ResourceType::Device.deterministic(&dev.ieee_address);
-                                let link_light =
-                                    ResourceType::Light.deterministic(&dev.ieee_address);
-
-                                let dev = Device {
-                                    product_data: DeviceProductData::hue_color_spot(),
-                                    metadata: Metadata::spot_bulb(name),
-                                    identify: json!({}),
-                                    services: vec![link_light.clone()],
-                                };
-
-                                self.map.insert(name.to_string(), link_light.rid);
-                                res.aux
-                                    .insert(link_light.rid, AuxData::new().with_topic(name));
-
-                                let mut light = Light::new(res.next_idv1(), link_device.clone());
-                                light.metadata.name = name.to_string();
-
-                                res.add(&link_device, Resource::Device(dev))?;
-                                res.add(&link_light, Resource::Light(light))?;
+                                log::info!(
+                                    "Adding light {:?}: [{}] ({id})",
+                                    dev.ieee_address,
+                                    dev.friendly_name
+                                );
+                                self.add_light(dev).await?;
                             }
                             _ => {}
                         }
@@ -172,87 +284,7 @@ impl Client {
                 Message::BridgeGroups(ref obj) => {
                     /* println!("{obj:#?}"); */
                     for grp in obj {
-                        let link_room = ResourceType::Room.deterministic(grp.id);
-                        let link_glight = ResourceType::GroupedLight.deterministic(grp.id);
-
-                        let children = grp
-                            .members
-                            .iter()
-                            .map(|f| ResourceType::Device.deterministic(&f.ieee_address))
-                            .collect();
-
-                        let mut services = vec![link_glight.clone()];
-
-                        for scn in &grp.scenes {
-                            let scene = Scene {
-                                actions: vec![],
-                                auto_dynamic: false,
-                                group: link_room.clone(),
-                                id_v1: Some("/scene/blob".to_string()),
-                                metadata: SceneMetadata {
-                                    appdata: None,
-                                    image: guess_scene_icon(&scn.name),
-                                    name: scn.name.to_string(),
-                                },
-                                palette: json!({
-                                    "color": [],
-                                    "dimming": [],
-                                    "color_temperature": [],
-                                    "effects": [],
-                                }),
-                                recall: None,
-                                speed: 0.5,
-                                status: Some(SceneStatus {
-                                    active: "inactive".to_string(),
-                                }),
-                            };
-
-                            let link_scene = ResourceType::Scene.deterministic((grp.id, scn.id));
-
-                            res.aux.insert(
-                                link_scene.rid,
-                                AuxData::new()
-                                    .with_topic(&grp.friendly_name)
-                                    .with_index(scn.id),
-                            );
-
-                            services.push(link_scene.clone());
-                            res.add(&link_scene, Resource::Scene(scene))?;
-                        }
-
-                        let room = Room {
-                            id_v1: Some(format!("/room/{}", grp.id)),
-                            children,
-                            metadata: Metadata::room(RoomArchetypes::Computer, &grp.friendly_name),
-                            services,
-                        };
-
-                        self.map
-                            .insert(grp.friendly_name.to_string(), link_glight.rid);
-                        res.add(&link_room, Resource::Room(room))?;
-
-                        let glight = GroupedLight {
-                            alert: json!({
-                                "action_values": [],
-                            }),
-                            color: LightColor::dummy(),
-                            color_temperature: ColorTemperature::dummy(),
-                            color_temperature_delta: json!({}),
-                            dimming: Dimming {
-                                brightness: 100.0,
-                                min_dim_level: None,
-                            },
-                            dimming_delta: json!({}),
-                            dynamics: json!({}),
-                            id_v1: Some(format!("/groups/{}", grp.id)),
-                            on: On { on: true },
-                            owner: link_room,
-                            signaling: json!({
-                                "signal_values": [],
-                            }),
-                        };
-
-                        res.add(&link_glight, Resource::GroupedLight(glight))?;
+                        self.add_group(grp).await?;
                     }
                 }
 
@@ -263,7 +295,7 @@ impl Client {
                 /* Message::BridgeState(ref obj) => { */
                 /*     /\* println!("{obj:#?}"); *\/ */
                 /* } */
-                Message::Other(ref obj) => {
+                Message::Other(obj) => {
                     println!("{:#?}", obj.topic);
                     if obj.topic.contains('/') {
                         continue;
@@ -274,24 +306,10 @@ impl Client {
                         continue;
                     };
 
-                    match res.get_resource_by_id(val)?.obj {
-                        Resource::Light(light) => {
-                            if let Err(e) = handle_light(val, &mut res, obj) {
-                                log::error!("FAIL: {e:?} in {obj:?}");
-                            }
-                        }
-                        Resource::GroupedLight(light) => {
-                            if let Err(e) = handle_grouped_light(val, &mut res, obj) {
-                                log::error!("FAIL: {e:?} in {obj:?}");
-                            }
-                        }
-                        _ => {}
-                    }
+                    self.handle_update(&val.clone(), obj.payload).await?;
                 }
                 _ => {}
             }
-
-            drop(res);
         }
         Ok(())
     }
