04de002ee55051853ede9f3587aa13dddce24f26
Christian Iversen <ci@iversenit.dk>
2024-08-10 18:05:17 +0200

    + Add DeviceProductData::guess_from_device() which attempts to construct DeviceProductData from z2m device info

--- src/hue/api/device.rs
+++ src/hue/api/device.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::hue::api::{Metadata, RType, ResourceLink};
+use crate::z2m;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Device {
@@ -50,6 +51,30 @@ impl DeviceProductData {
             product_archetype: DeviceArchetype::BridgeV2,
             product_name: "Hue Bridge".to_string(),
             software_version: "1.60.1960149090".to_string(),
+        }
+    }
+
+    #[must_use]
+    pub fn guess_from_device(dev: &z2m::api::Device) -> Self {
+        fn str_or_unknown(name: &Option<String>) -> String {
+            name.clone().unwrap_or_else(|| String::from("<unknown>"))
+        }
+
+        let model_id = str_or_unknown(&dev.model_id);
+        let manufacturer_name = str_or_unknown(&dev.manufacturer);
+        let certified = manufacturer_name == Self::SIGNIFY_MANUFACTURER_NAME;
+        let product_name = dev.friendly_name.clone();
+        let software_version = str_or_unknown(&dev.software_build_id);
+
+        let product_archetype = DeviceArchetype::SpotBulb;
+
+        Self {
+            model_id,
+            manufacturer_name,
+            product_name,
+            product_archetype,
+            certified,
+            software_version,
         }
     }
 }
