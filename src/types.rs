4237612081ef754aee8f560fdbec46ea0a706816
Christian Iversen <ci@iversenit.dk>
2024-08-10 22:44:13 +0200

    + Add constant for D65 white point on XY struct (to be used as reasonable default)

--- src/types.rs
+++ src/types.rs
@@ -7,6 +7,11 @@ pub struct XY {
 }
 
 impl XY {
+    pub const D65_WHITE_POINT: Self = Self {
+        x: 0.31271,
+        y: 0.32902,
+    };
+
     #[must_use]
     pub const fn new(x: f64, y: f64) -> Self {
         Self { x, y }
