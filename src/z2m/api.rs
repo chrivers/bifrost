05e6884a984bc1d2fdfdcd66e23dd0b8ec6e8b59
Christian Iversen <ci@iversenit.dk>
2024-08-24 16:04:56 +0200

    * Silence a clippy lint

--- src/z2m/api.rs
+++ src/z2m/api.rs
@@ -475,7 +475,7 @@ pub struct ExposeNumeric {
 
 impl ExposeNumeric {
     #[must_use]
-    #[allow(clippy::cast_sign_loss)]
+    #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
     pub fn extract_mirek_schema(&self) -> Option<MirekSchema> {
         if self.unit.as_deref() == Some("mired") {
             if let (Some(min), Some(max)) = (self.value_min, self.value_max) {
