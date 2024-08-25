9548566d510bfa7074bfa35b66a6bb74c8495183
Christian Iversen <ci@iversenit.dk>
2024-08-25 22:53:38 +0200

    + Add normalize-hue-get example for collaboration with diyHue developers

---
+++ examples/normalize-hue-get.rs
@@ -0,0 +1,25 @@
+#![allow(unused_variables)]
+
+use std::io::stdin;
+
+use bifrost::error::ApiResult;
+use bifrost::hue::api::ResourceRecord;
+
+#[tokio::main]
+async fn main() -> ApiResult<()> {
+    pretty_env_logger::init();
+
+    for (index, line) in stdin().lines().enumerate() {
+        let data: Result<ResourceRecord, _> = serde_json::from_str(&line?);
+
+        let Ok(msg) = data else {
+            let err = data.unwrap_err();
+            log::error!("Parse error at line {} column {}", err.line(), err.column());
+            continue;
+        };
+
+        println!("{}", serde_json::to_string(&msg)?);
+    }
+
+    Ok(())
+}
