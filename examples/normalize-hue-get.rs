22ab813717749644ddb248fe6e9e3e20825fbc27
Christian Iversen <ci@iversenit.dk>
2024-09-14 10:26:06 +0200

    * Reworked normalize-hue-get example, and make it able to handle both raw and linedump data from hue bridges

--- examples/normalize-hue-get.rs
+++ examples/normalize-hue-get.rs
@@ -1,15 +1,81 @@
 #![allow(unused_variables)]
 
-use std::io::stdin;
+use std::io::{stdin, Stdin};
 
 use bifrost::error::ApiResult;
 use bifrost::hue::api::ResourceRecord;
 
 use json_diff_ng::compare_serde_values;
-use serde_json::Value;
+use serde_json::{de::IoRead, Deserializer, StreamDeserializer, Value};
 
 fn false_positive((a, b): &(&Value, &Value)) -> bool {
     a.is_number() && b.is_number() && a.as_f64() == b.as_f64()
+}
+
+/*
+ * Handle both "raw" hue bridge dumps, and the "linedump" format that is
+ * generally easier to work with.
+ *
+ * For each element in the input, if it is an object, look for a ".data" array,
+ * and if found, iterate over that. Otherwise, assume the whole object is what
+ * we are parsing.
+ */
+fn extract(
+    stream: StreamDeserializer<IoRead<Stdin>, Value>,
+) -> impl Iterator<Item = (usize, Value)> + '_ {
+    stream
+        .flat_map(|val| {
+            val.as_ref()
+                .unwrap()
+                .as_object()
+                .and_then(|obj| obj.get("data"))
+                .map(|data| data.as_array().unwrap().to_owned())
+                .unwrap_or_else(|| vec![val.unwrap()])
+        })
+        .enumerate()
+}
+
+fn compare(before: &Value, after: &Value, msg: ResourceRecord) -> ApiResult<()> {
+    let diffs = compare_serde_values(before, after, true, &[]).unwrap();
+    let all_diffs = diffs.all_diffs();
+
+    if !all_diffs
+        .iter()
+        .any(|x| x.1.values.map(|q| !false_positive(&q)).unwrap_or(true))
+    {
+        return Ok(());
+    }
+
+    log::error!("Difference detected on {:?}", msg.obj.rtype());
+    eprintln!("--------------------------------------------------------------------------------");
+    println!("{}", serde_json::to_string(before)?);
+    eprintln!("--------------------------------------------------------------------------------");
+    println!("{}", serde_json::to_string(&msg)?);
+    eprintln!("--------------------------------------------------------------------------------");
+    for (d_type, d_path) in all_diffs {
+        if let Some(ref ab) = d_path.values {
+            if false_positive(ab) {
+                continue;
+            }
+        }
+        match d_type {
+            json_diff_ng::DiffType::LeftExtra => {
+                eprintln!(" - {d_path}");
+            }
+            json_diff_ng::DiffType::Mismatch => {
+                eprintln!(" * {d_path}");
+            }
+            json_diff_ng::DiffType::RightExtra => {
+                eprintln!(" + {d_path}");
+            }
+            json_diff_ng::DiffType::RootMismatch => {
+                eprintln!("{d_type}: {d_path}");
+            }
+        }
+    }
+    eprintln!();
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -19,61 +85,22 @@ async fn main() -> ApiResult<()> {
         .parse_default_env()
         .init();
 
-    for (index, line) in stdin().lines().enumerate() {
-        let line = line?;
-        let before: Value = serde_json::from_str(&line)?;
-        let data: Result<ResourceRecord, _> = serde_json::from_str(&line);
+    let stream = Deserializer::from_reader(stdin()).into_iter::<Value>();
+
+    for (index, obj) in extract(stream) {
+        let before = obj;
+        let data: Result<ResourceRecord, _> = serde_json::from_value(before.clone());
 
         let Ok(msg) = data else {
             let err = data.unwrap_err();
-            log::error!("Parse error {err:?} (stdin line {})", index + 1);
-            eprintln!("{}", &line);
+            log::error!("Parse error {err:?} (object index {})", index);
+            eprintln!("{}", &serde_json::to_string(&before)?);
             continue;
         };
 
         let after = serde_json::to_value(&msg)?;
 
-        let diffs = compare_serde_values(&before, &after, true, &[]).unwrap();
-        if diffs
-            .all_diffs()
-            .iter()
-            .any(|x| x.1.values.map(|q| !false_positive(&q)).unwrap_or(true))
-        {
-            log::error!("Difference detected on {:?}", msg.obj.rtype());
-            println!(
-                "--------------------------------------------------------------------------------"
-            );
-            println!("{}", &line);
-            println!(
-                "--------------------------------------------------------------------------------"
-            );
-            println!("{}", serde_json::to_string(&msg)?);
-            println!(
-                "--------------------------------------------------------------------------------"
-            );
-            for (d_type, d_path) in diffs.all_diffs() {
-                if let Some(ref ab) = d_path.values {
-                    if false_positive(ab) {
-                        continue;
-                    }
-                }
-                match d_type {
-                    json_diff_ng::DiffType::LeftExtra => {
-                        eprintln!(" - {d_path}");
-                    }
-                    json_diff_ng::DiffType::Mismatch => {
-                        eprintln!(" * {d_path}");
-                    }
-                    json_diff_ng::DiffType::RightExtra => {
-                        eprintln!(" + {d_path}");
-                    }
-                    json_diff_ng::DiffType::RootMismatch => {
-                        eprintln!("{d_type}: {d_path}");
-                    }
-                }
-            }
-            eprintln!();
-        }
+        compare(&before, &after, msg)?;
     }
 
     Ok(())
