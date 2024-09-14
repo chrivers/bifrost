849c5615663ceba6b29a29dca034a402476fe00e
Christian Iversen <ci@iversenit.dk>
2024-09-14 16:20:33 +0200

    + Implement /licenses/ endpoints

--- src/routes/licenses.rs
+++ src/routes/licenses.rs
@@ -1,5 +1,7 @@
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Json, Router};
+use itertools::Itertools;
 use serde_json::{json, Value};
 
 use crate::server::appstate::AppState;
@@ -9,7 +11,42 @@ async fn packages() -> Json<Value> {
 }
 
 async fn hardcoded() -> Json<Value> {
-    Json(json!([]))
+    Json(json!([{
+        "Attributions": [],
+        "Package": "bifrost",
+        "SPDX-License-Identifiers": [
+            "GPL-3.0"
+        ],
+        "SourceLinks": [
+            "https://github.com/chrivers/bifrost",
+        ],
+        "Version": "0.9",
+        "Website": "https://github.com/chrivers/bifrost",
+        "licenses": {
+            "GPL-3.0": "gpl-3.0.txt",
+        }
+    }]))
+}
+
+async fn license() -> impl IntoResponse {
+    const LICENSE: &str = include_str!("../../LICENSE");
+
+    let split = LICENSE
+        .find("Preamble")
+        .expect("License file must have preamble");
+
+    /* a bit of string trickery to make license render nicely in hue app */
+    format!(
+        "{}{}",
+        &LICENSE[..split]
+            .split("\n\n ")
+            .map(|s| s.replace("\n ", " "))
+            .join("\n\n"),
+        &LICENSE[split..]
+            .split("\n\n  ")
+            .map(|s| s.replace("\n    ", "\n").replace('\n', " "))
+            .join("\n\n")
+    )
 }
 
 async fn rust_packages() -> Json<Value> {
@@ -21,4 +58,5 @@ pub fn router() -> Router<AppState> {
         .route("/packages.json", get(packages))
         .route("/hardcoded.json", get(hardcoded))
         .route("/rust-packages.json", get(rust_packages))
+        .route("/gpl-3.0.txt", get(license))
 }
