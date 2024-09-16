bdc13696b1dbf53b82517f16bdce77fe399df032
Christian Iversen <ci@iversenit.dk>
2024-09-16 15:49:43 +0200

    + Implement unit tests for new serde helpers

--- src/z2m/serde_util.rs
+++ src/z2m/serde_util.rs
@@ -62,3 +62,69 @@ pub mod struct_or_false {
     pub use super::deserialize_struct_or_false as deserialize;
     pub use super::serialize_struct_or_false as serialize;
 }
+
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+    use serde_json::{from_str, to_string};
+
+    use crate::error::ApiResult;
+
+    #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+    struct Foo {
+        #[serde(with = "super::struct_or_false")]
+        foo: Option<Bar>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+    struct Bar {
+        bar: u32,
+    }
+
+    const FOO_NONE: Foo = Foo { foo: None };
+    const FOO_SOME: Foo = Foo {
+        foo: Some(Bar { bar: 42 }),
+    };
+
+    const FOO_NONE_STR: &str = r#"{"foo":false}"#;
+    const FOO_SOME_STR: &str = r#"{"foo":{"bar":42}}"#;
+    const FOO_TRUE: &str = r#"{"foo":true}"#;
+    const FOO_LIST: &str = r#"{"foo":[42]}"#;
+
+    #[test]
+    pub fn serialize_none() -> ApiResult<()> {
+        assert_eq!(to_string(&FOO_NONE)?, FOO_NONE_STR);
+        Ok(())
+    }
+
+    #[test]
+    pub fn serialize_some() -> ApiResult<()> {
+        assert_eq!(to_string(&FOO_SOME)?, FOO_SOME_STR);
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn deserialize_false() -> ApiResult<()> {
+        assert_eq!(from_str::<Foo>(FOO_NONE_STR)?, FOO_NONE);
+        Ok(())
+    }
+
+    #[test]
+    pub fn deserialize_struct() -> ApiResult<()> {
+        assert_eq!(from_str::<Foo>(FOO_SOME_STR)?, FOO_SOME);
+        Ok(())
+    }
+
+    #[test]
+    pub fn deserialize_true() {
+        /* must return error */
+        from_str::<Foo>(FOO_TRUE).unwrap_err();
+    }
+
+    #[test]
+    pub fn deserialize_list() {
+        /* must return error */
+        from_str::<Foo>(FOO_LIST).unwrap_err();
+    }
+}
